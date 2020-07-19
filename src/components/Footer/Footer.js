e768e314560fcd41b39a8993f8ccf404a2c3b6ef
RFJ <rfj.braunstingl@gmail.com>
2020-07-19 16:20:25 +0200

    fix styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
+import css from "./Footer.module.css";
 
-const Footer = (props) => {
-    console.log(process.env.REACT_APP_VERSION)
-    return (
-    <div>
-        <span>{process.env.REACT_APP_VERSION}</span>
-    </div>
-)
-    }
-export default Footer
+const Footer = () => (
+  <div className={css.Footer}>
+    <span>{process.env.REACT_APP_VERSION}</span>
+  </div>
+);
+
+export default Footer;
