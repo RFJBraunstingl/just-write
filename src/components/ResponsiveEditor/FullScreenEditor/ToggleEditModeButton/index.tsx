fd9371feeee04ed2b7bc20dd3d5850969d4a3e1e
RFJBraunstingl <rfj.braunstingl@gmail.com>
2021-06-24 20:53:33 +0200

    add mode toggle button

---
+++ src/components/ResponsiveEditor/FullScreenEditor/ToggleEditModeButton/index.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+import styles from './styles.module.css'
+import ChangeToTextImage from "../../../../assets/img/image_white.svg";
+import ChangeToPreviewImage from "../../../../assets/img/image_filled.svg";
+
+export enum Mode {
+    EDIT,
+    VIEW
+}
+
+interface Props {
+    onModeChange?: (newMode: Mode) => void
+}
+
+const ToggleEditModeButton = ({onModeChange = () => {}}: Props) => {
+
+    const [isEditModeActive, setEditModeActive] = useState<boolean>(true)
+
+    const handleClick = () => {
+        setEditModeActive(!isEditModeActive)
+    }
+
+    useEffect(() => {
+        const currentMode = isEditModeActive ? Mode.EDIT : Mode.VIEW
+        onModeChange(currentMode)
+    }, [isEditModeActive])
+
+    return <div className={styles.Wrapper} onClick={handleClick}>
+        <img src={isEditModeActive ? ChangeToPreviewImage : ChangeToTextImage}  alt='button to change mode' />
+    </div>
+}
+
+export default ToggleEditModeButton
--- src/components/ResponsiveEditor/FullScreenEditor/index.tsx
+++ src/components/ResponsiveEditor/FullScreenEditor/index.tsx
@@ -2,16 +2,22 @@ import React, {useState} from "react";
 import {EditorProps} from "../EditorProps";
 import MarkdownInput from "../../MarkdownInput/MarkdownInput";
 import MarkdownPreview from "../../MarkdownPreview/MarkdownPreview";
+import styles from "./styles.module.css";
+import ToggleEditModeButton from "./ToggleEditModeButton";
 
 const FullScreenEditor = ({text, onTextUpdate}: EditorProps) => {
 
     const [isEditMode, setEditMode] = useState<boolean>(true)
-
-    if (isEditMode) {
-        return <MarkdownInput onTextChange={onTextUpdate}/>
-    } else {
-        return <MarkdownPreview markdownText={text}/>
-    }
+    return (
+        <>
+            <div className={styles.Wrapper}>
+                {isEditMode ?
+                    <MarkdownInput onTextChange={onTextUpdate}/> :
+                    <MarkdownPreview markdownText={text}/>}
+            </div>
+            <ToggleEditModeButton />
+        </>
+    )
 }
 
 export default FullScreenEditor
